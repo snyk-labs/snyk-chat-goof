3f604c7b96d03e832d43b2d8b79346ce3647bffb
Micah Silverman <micah@afitnerd.com>
2022-03-22 19:42:11 -0400

    Initial Revision

---
+++ src/frontend/components/room/ChatContent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-list
+      two-line
+      subheader
+    >
+      <v-list-item
+        v-for="(message, index) in messages"
+        :key="index"
+      >
+        <template v-if="message.type == 'MESSAGE'">
+          <v-list-item-avatar>
+            <v-avatar color="#ff9b20">
+              <span class="white--text headline">{{ message.userName.charAt(0) }}</span>
+            </v-avatar>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="message.userName"></v-list-item-title>
+            <v-list-item-subtitle v-html="markit(message.message)"></v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+        <template v-else>
+          <v-divider inset></v-divider>
+          <v-subheader inset>{{ userEvent(message) }}</v-subheader>
+          <v-divider inset></v-divider>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+
+marked.setOptions({ sanitize: true });
+
+export default {
+  computed: {
+    messages() {
+      const roomId = this.$route.params.roomId;
+      return this.$store.getters["main/roomMessages"](roomId);
+    }
+  },
+  methods: {
+    userEvent(message) {
+      if (message.type == "JOIN") return message.userName + " joined!";
+      if (message.type == "LEAVE") return message.userName + " left!";
+      return "uknown message type";
+    },
+    markit(message) {
+      return marked(message);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/frontend/layouts/chat.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      :value="sidebar"
+      app
+      mobile-breakpoint="600"
+    >
+      <new-room></new-room>
+      <v-divider></v-divider>
+      <room-list></room-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-row justify="center">
+        <v-toolbar-items>
+          <v-btn
+            text
+            to="/"
+            large
+          >Home</v-btn>
+          <v-btn
+            text
+            to="/chat"
+            large
+          >Chat</v-btn>
+          <v-btn
+            text
+            to="/about"
+            large
+          >About</v-btn>
+        </v-toolbar-items>
+      </v-row>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-layout
+          align-top
+          justify-center
+          mt-12
+        >
+          <nuxt />
+        </v-layout>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019 <a
+          href="http://kojotdev.com"
+          target="_blank"
+          class="white--text"
+        >kojotdev.com</a></span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import NewRoom from "@/components/room/NewRoom.vue";
+import RoomList from "@/components/room/RoomList.vue";
+export default {
+  components: {
+    NewRoom,
+    RoomList
+  },
+  computed: {
+    sidebar() {
+      return this.$store.getters["main/sidebar"];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
